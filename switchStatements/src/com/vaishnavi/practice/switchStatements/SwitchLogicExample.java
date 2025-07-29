876d920f63426d0e38039cf968fb84e65b285c03
Vaishnavi Chevale <130659974+vaishnavi890@users.noreply.github.com>
2025-07-29 11:47:25 +0530

    initial import

---
+++ switchStatements/src/com/vaishnavi/practice/switchStatements/SwitchLogicExample.java
@@ -0,0 +1,48 @@
+package com.vaishnavi.practice.switchStatements;
+
+import java.util.Scanner;
+
+public class SwitchLogicExample {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter first number:");
+        double num1 = sc.nextDouble();
+
+        System.out.println("Enter second number:");
+        double num2 = sc.nextDouble();
+
+        System.out.println("Choose operation:");
+        System.out.println("1: Add");
+        System.out.println("2: Subtract");
+        System.out.println("3: Multiply");
+        System.out.println("4: Divide");
+
+        int choice = sc.nextInt();
+
+        switch (choice) {
+            case 1:
+                System.out.println("Result: " + (num1 + num2));
+                break;
+            case 2:
+                System.out.println("Result: " + (num1 - num2));
+                break;
+            case 3:
+                System.out.println("Result: " + (num1 * num2));
+                break;
+            case 4:
+                if (num2 != 0) {
+                    System.out.println("Result: " + (num1 / num2));
+                } else {
+                    System.out.println("Cannot divide by zero.");
+                }
+                break;
+            default:
+                System.out.println("Invalid choice.");
+        }
+
+        sc.close();
+    }
+}
+
+
