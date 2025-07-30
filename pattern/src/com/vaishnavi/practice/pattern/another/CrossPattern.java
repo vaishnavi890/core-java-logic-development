ccc9e55c25c7e8a362cf6d52d3240568610a3bb9
Vaishnavi Chevale <130659974+vaishnavi890@users.noreply.github.com>
2025-07-30 12:42:35 +0530

    initial import

---
+++ pattern/src/com/vaishnavi/practice/pattern/another/CrossPattern.java
@@ -0,0 +1,18 @@
+package com.vaishnavi.practice.pattern.another;
+
+public class CrossPattern {
+    public static void main(String[] args) {
+        int size = 7;
+        for (int i = 1; i <= size; i++) {
+            for (int j = 1; j <= size; j++) {
+                if (j == i || j == size - i + 1)
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+
