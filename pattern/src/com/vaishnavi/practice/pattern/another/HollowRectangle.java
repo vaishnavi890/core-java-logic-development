6b4970cb3a6852d653d4a5edd38c538fd7ed3a77
Vaishnavi Chevale <130659974+vaishnavi890@users.noreply.github.com>
2025-07-30 12:28:37 +0530

    initial import

---
+++ pattern/src/com/vaishnavi/practice/pattern/another/HollowRectangle.java
@@ -0,0 +1,18 @@
+package com.vaishnavi.practice.pattern.another;
+
+public class HollowRectangle {
+    public static void main(String[] args) {
+        int rows = 5, cols = 7;
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= cols; j++) {
+                if (i == 1 || i == rows || j == 1 || j == cols)
+                    System.out.print("* ");
+                else
+                    System.out.print("  ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+
